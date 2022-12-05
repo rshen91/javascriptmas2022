f7260b370d02ceceb8464e72a5c64416ea22f81b
Rachel <rshen@elastic.co>
2022-12-04 19:27:14 -0700

    day 5 solution

---
+++ day5.js
@@ -0,0 +1,27 @@
+import products from "./data.js";
+
+/*
+   It's the day after Halloween 🎃 and all the candy is on sale!
+   
+   To buy up all the candy, use map() and filter() to put all the
+   candy into a `shoppingCart` array. 
+   
+   The new array should contain only the item and the price, like
+   this: 
+   
+   Expected output: 
+   [
+       {item: "🍭", price: 2.99},
+       {item: "🍫", price: 1.99}, 
+       {item: "🍬", price: 0.89}
+    ]
+*/
+
+function getSaleItems(data){
+    const shoppingCart = data.filter((food) => {
+        return food.type === 'sweet'
+    })
+    return shoppingCart.map(({item, price}) => { return {item, price}})
+};
+
+console.log(getSaleItems(products))
