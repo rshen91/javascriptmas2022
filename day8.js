7d7c1439c8d715e09c811e108a5b9c3b44773ebf
rshen91 <rshen@elastic.co>
2022-12-08 07:44:31 -0700

    day 8 solution

---
+++ day8.js
@@ -0,0 +1,54 @@
+const validTime = (str) => {
+    const [hours, mins] = str.split(':')
+    const validHours = 24 >= parseInt(hours) 
+    const validMins = 60 > parseInt(mins)
+    return validHours && validMins
+}
+
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
